ad4c712313059e3d75a057d52d348f78b7fe8649
kimo3412 <2034409413@qq.com>
2026-06-04 14:06:20 +0800

    更新

--- Code/sql/V10__fix_default_banner_image_urls.sql
+++
@@ -1,8 +0,0 @@
--- Fix default banner URLs that pointed to non-existent uploaded files.
-UPDATE ds_banner
-SET image_url = CASE
-    WHEN image_url = '/uploads/banners/banner1.jpg' THEN '/static/images/home-banner-couture.png'
-    WHEN image_url = '/uploads/banners/banner2.jpg' THEN '/static/images/home-banner-leather.png'
-    ELSE image_url
-END
-WHERE image_url IN ('/uploads/banners/banner1.jpg', '/uploads/banners/banner2.jpg');
